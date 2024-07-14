a046d009ba439c7d1a9656e0a5f1ff64d59e31cb
zebonair <emir.muratcaus97@gmail.com>
2024-07-14 20:14:57 +0200

    weather-sns: spring boot setup intializer

---
+++ src/main/java/com/weathernotification/weather_sns/WeatherSnsApplication.java
@@ -0,0 +1,13 @@
+package com.weathernotification.weather_sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherSnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherSnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weathernotification/weather_sns/WeatherSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weathernotification.weather_sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
