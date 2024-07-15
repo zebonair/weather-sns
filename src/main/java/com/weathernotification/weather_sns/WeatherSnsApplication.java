1addf8b32a3f63ba4d1d03d7dcc92396740a2384
zebonair <emir.muratcaus97@gmail.com>
2024-07-15 12:11:09 +0200

    test main protection

--- src/main/java/com/weathernotification/weather_sns/WeatherSnsApplication.java
+++ src/main/java/com/weathernotification/weather_sns/WeatherSnsApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WeatherSnsApplication {
 
     public static void main(String[] args) {
-        //123
         SpringApplication.run(WeatherSnsApplication.class, args);
     }
 }
