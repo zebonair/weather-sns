9f0b140e726f1860bc1241a1e9712d450488cb15
zebonair <emir.muratcaus97@gmail.com>
2024-07-15 12:01:05 +0200

    weather-sns: Configure linter, fix build yml file

--- src/main/java/com/weathernotification/weather_sns/WeatherSnsApplication.java
+++ src/main/java/com/weathernotification/weather_sns/WeatherSnsApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeatherSnsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeatherSnsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherSnsApplication.class, args);
+    }
 }
