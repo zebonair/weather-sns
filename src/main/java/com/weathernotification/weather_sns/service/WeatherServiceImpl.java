ee4c6a6d3469b453a82190947770952fbc5396ef
zebonair <emir.muratcaus97@gmail.com>
2024-08-05 01:16:55 +0200

    WSNS#21: Refactor usage of GCP secrets (use api key without SM)

--- src/main/java/com/weathernotification/weather_sns/service/WeatherServiceImpl.java
+++ src/main/java/com/weathernotification/weather_sns/service/WeatherServiceImpl.java
@@ -1,9 +1,6 @@
 package com.weathernotification.weather_sns.service;
 
-import com.google.cloud.secretmanager.v1.AccessSecretVersionRequest;
-import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
-import com.google.cloud.secretmanager.v1.SecretVersionName;
-import com.weathernotification.weather_sns.config.GCPConfig;
+import com.weathernotification.weather_sns.config.WeatherAPIConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -13,42 +10,27 @@ import org.springframework.web.client.RestTemplate;
 public class WeatherServiceImpl implements WeatherService {
 
     private final RestTemplate restTemplate;
-    private final String weatherApiUrl;
     private final String weatherApiKey;
+    private final String weatherApiUrlCurrent;
     private static final Logger logger = LoggerFactory.getLogger(WeatherServiceImpl.class);
 
-    public WeatherServiceImpl(RestTemplate restTemplate, GCPConfig gcpconfig) throws Exception {
+    public WeatherServiceImpl(RestTemplate restTemplate, WeatherAPIConfig weatherApiConfig) {
         this.restTemplate = restTemplate;
-        this.weatherApiUrl = gcpconfig.getApiUrl();
-
-        String projectId = gcpconfig.getProjectId();
-        String secretId = gcpconfig.getSecretName();
-
-        this.weatherApiKey = getSecret(projectId, secretId);
+        this.weatherApiKey = weatherApiConfig.getWeatherApiKey();
+        this.weatherApiUrlCurrent = weatherApiConfig.getWeatherApiUrl();
     }
 
     @Override
     public String getWeather(String location) {
         try {
             String url =
-                    String.format("%s?key=%s&q=%s&aqi=no", weatherApiUrl, weatherApiKey, location);
+                    String.format(
+                            "%s?key=%s&q=%s&aqi=no", weatherApiUrlCurrent, weatherApiKey, location);
             logger.info("Successfully fetched weather data for location: {}", location);
             return restTemplate.getForObject(url, String.class);
         } catch (Exception e) {
             logger.error("Error fetching weather data for location: {}", location, e);
             throw e;
-        }
-    }
-
-    private String getSecret(String projectId, String secretNameme) throws Exception {
-        try (SecretManagerServiceClient client = SecretManagerServiceClient.create()) {
-            SecretVersionName secretVersionName =
-                    SecretVersionName.of(projectId, secretNameme, "latest");
-            AccessSecretVersionRequest request =
-                    AccessSecretVersionRequest.newBuilder()
-                            .setName(secretVersionName.toString())
-                            .build();
-            return client.accessSecretVersion(request).getPayload().getData().toStringUtf8();
         }
     }
 }
